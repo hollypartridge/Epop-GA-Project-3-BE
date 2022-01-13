fb00dfb1dac8c78e1aff43a93e7a86993e7907ca
hollypartridge <holpartridge@gmail.com>
2022-01-13 14:23:52 +0000

    ready to deploy

--- db/helpers.js
+++ db/helpers.js
@@ -8,15 +8,23 @@ export function connectToDB() {
     family: 4,
   })
 }
-export function truncateDB(){
+
+export function truncateDb() {
+  // return mongoose.connection.db.dropDatabase()
+
+  //* If having issues dropping db locally, comment this ^^^ line above back om  and COMMENT OUT the if statement below !
+
   if (mongoose.connection.readyState !== 0) {
     const { collections } = mongoose.connection
+
     const promises = Object.keys(collections).map(collection => {
       return mongoose.connection.collection(collection).deleteMany({})
     })
+
     return Promise.all(promises)
   }
 }
+
 export function disconnectDB() {
   if (mongoose.connection.readyState !== 0) {
     return mongoose.disconnect()
